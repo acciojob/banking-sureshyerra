451c1b360e1abac58e85a2dcdc45257debcfb4cd
unknown <sureshyerr897@gmail.com>
2023-03-29 19:11:56 +0530

    done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,6 +2,8 @@ package com.driver;
 
 public class Main {
     public static void main(String[] args) {
+        BankAccount bankAccount = new BankAccount("suresh",2000,500);
+        bankAccount.deposit(700);
 
     }
 }
